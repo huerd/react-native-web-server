e8bca414ec1559a7e941b06c3248e541e8ab9a76
Lex Sapkota <lexsapkota@gmail.com>
2019-05-08 12:37:55 +1000

    React Native 57 - Android fixes

--- webserver/packageMobile.sh
+++ webserver/packageMobile.sh
@@ -3,6 +3,10 @@ set -e
 IOS_DEST=../ios/DerivedData/ReactNativeWebServer/Build/Products/Debug-iphonesimulator
 IOS_FRAMEWORK_FILE=Webserver.framework
 
+ANDROID_DEST=../android/app/libs/
+ANDROID_FRAMEWORK_FILE=webserver.aar
+ANDROID_FRAMEWORK_FILE_SOURCES=webserver-sources.jar
+
 if [ $# -eq 0 ]
 then
   echo "Specify a target: ios, android"
@@ -17,3 +21,14 @@ if [ $1 = "ios" ]; then
   fi
   mv $IOS_FRAMEWORK_FILE $IOS_DEST
 fi
+
+if [ $1 = "android" ]; then
+  if [ ! -f "$ANDROID_DEST/$ANDROID_FRAMEWORK_FILE" ]; then
+    rm -rf "$ANDROID_DEST/$ANDROID_FRAMEWORK_FILE"
+    cp -R $ANDROID_FRAMEWORK_FILE $ANDROID_DEST
+    cp -R $ANDROID_FRAMEWORK_FILE_SOURCES $ANDROID_DEST
+  fi
+  rm -rf $ANDROID_FRAMEWORK_FILE
+  rm -rf $ANDROID_FRAMEWORK_FILE_SOURCES
+fi
+
